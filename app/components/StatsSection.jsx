41dcc0e8f719dd2189c0cde24b1a138d7c6c9cb5
RaiYashPramod <raiyashpramod16@gmail.com>
2024-02-12 23:39:05 +0530

    actual stats

--- app/components/StatsSection.jsx
+++ app/components/StatsSection.jsx
@@ -5,8 +5,8 @@ const StatsSection = () => {
   return (
     <>
     <Grid>
-      <Card><CardText>18</CardText><CardSubText>Years</CardSubText></Card>
-      <Card><CardText>3+</CardText><CardSubText>Projects</CardSubText></Card>
+      <Card><CardText>4</CardText><CardSubText>Years</CardSubText></Card>
+      <Card><CardText>10+</CardText><CardSubText>Projects</CardSubText></Card>
       <Card><CardText>8</CardText><CardSubText>Services</CardSubText></Card>
       <Card><CardText>100%</CardText><CardSubText>Satisfaction</CardSubText></Card>
     </Grid>
